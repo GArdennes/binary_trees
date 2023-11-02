653c5125a6d8a2093b85d3e654c0792c035acabd
GArdennes <kevin.afachao@gmail.com>
2023-11-02 08:05:48 -0700

    Task 13

--- binary_tree_nodes.c
+++ binary_tree_nodes.c
@@ -7,12 +7,13 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-if (tree == NULL)
-return (0);
+	size_t left_node, right_node;
 
-if (tree->left != NULL || tree->right != NULL)
-return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
-
-return (0);
+	if (tree == NULL)
+		return (0);
+	if (tree->left != NULL || tree->right !=  NULL)
+		return (1);
+	left_node = binary_tree_nodes(tree->left);
+	right_node = binary_tree_nodes(tree->right);
+	return (left_node + right_node);
 }
-
